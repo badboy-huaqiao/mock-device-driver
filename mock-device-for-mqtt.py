0bf3e940051af4950c77b27c31095470ec9575f5
Huaqiao Zhang <huaqiaoz@vmware.com>
2019-04-09 00:54:30 +0800

    更新mock device脚本，添加主动发送数据功能
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- mock-device-for-mqtt.py
+++ mock-device-for-mqtt.py
@@ -5,6 +5,48 @@
 
 import paho.mqtt.client as mqtt
 import json
+import time
+import Queue
+import threading
+
+globalQueue = Queue.Queue()
+
+def send_data():
+    #java版本, name的值为添加的设备名
+    var data = {"randnum":520.1314,"name":""}
+
+    #go版本, name的值为添加的设备名, go版本的区别是必须带上cmd字段
+    #var data = {"randnum":520.1314,"name":"","cmd":"randnum"}
+
+    print("sending data actively! " + json.dumps(data))
+    client.publish("DataTopic",json.dumps(data) , qos=0, retain=False)
+
+class SendDataActiveServer(threading.Thread):
+     def __init__(self,threadID,name,queue):
+         super(SendDataActiveServer,self).__init__()
+         self.threadID = threadID
+         self.name = name
+         self.queue = queue
+
+     def run(self):
+         var active = false
+         active = self.getItemFromQueue()
+
+         while 1==1 :
+           if active:
+              send_data()
+              time.sleep(1)
+              active = self.getItemFromQueue()
+           else:
+              time.sleep(1)
+              active = self.getItemFromQueue()
+
+     def getItemFromQueue(self):
+         try:
+           return self.queue.get(block=False)
+         except Queue.Empty:
+           #quene.get()方法在队列中为空是返回异常，捕获异常返回false表示没有收到任何消息
+           return false
 
 #当接收到命令，响应命令
 def on_message(client, userdata, msg):
@@ -16,9 +58,21 @@ def on_message(client, userdata, msg):
           d['message'] = "Are you ok?"
        elif d['method'] == "set":
           d['result'] = "set successed."
+
     if d['cmd'] == "ping":
        print("This is ping cmd")
        d['ping'] = "pong"
+
+    if d['cmd'] == "randnum":
+       print("This is randnum cmd")
+       d['randnum'] = 520.1314
+
+    if d['cmd'] == "collect" && d['method'] == "set":
+       print("This is collect cmd")
+       d['result'] = "set successed."
+       #param的值是true或false
+       globalQueue.put(d['param'])
+
 
     print(json.dumps(d))
     client.publish("ResponseTopic", json.dumps(d))
@@ -34,5 +88,9 @@ client.on_message = on_message
 client.on_connect = on_connect
 
 client.connect("192.168.56.4", 1883, 60)
+
+#开始独立线程用于主动发送数据
+thread = SendDataActiveServer("Thread-1", "SendDataServerThread", globalQueue)
+thread.start()
 
 client.loop_forever()
